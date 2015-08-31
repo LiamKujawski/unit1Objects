060d5281ef666f3cf50ccb8878b102794fcc7974
LiamKujawski <ldkujawski@NN121-HA-20.HS.sd203.org>
2015-08-31 10:28:02 -0500

    String and Rectangle prompts

---
+++ Chapter 2 Practice/String1.java
@@ -0,0 +1,15 @@
+public class String1
+{
+    public static void main(String[] args)
+    {
+        String greeting = "Hellow, World";
+        int n = greeting.length();
+        
+        System.out.println("length of string: " + n);
+        
+        String river = "Mississippi";
+        String bigRiver = river.toUpperCase();
+        System.out.println( "river: " + river + "\nBigRiver: " + bigRiver );
+        
+    }
+}

a6a869969ab65758b618b6a26540b591a8603969
LiamKujawski <ldkujawski@NN121-HA-20.HS.sd203.org>
2015-09-02 10:30:26 -0500

    TurtleSummative

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.util.Random;
 
 public class TurtleWorld
 {
@@ -6,22 +7,53 @@ public class TurtleWorld
     {
         World turtleWorld = new World();
         Turtle turtle = new Turtle(turtleWorld);
+        Turtle turtle2 = new Turtle(turtleWorld);
+        Turtle turtle3 = new Turtle(turtleWorld);
         turtle.setPenColor(Color.BLUE);
         turtle.penDown();
         turtle.setShellColor(Color.BLUE);
-       // var turtle = true;
-        //while (var turtle = true);{
-           // turtle.forward(50);
-            //t//urtle.turnLeft();
-            //turtle.forward(50);
-            //turtle.turnLeft();
-            //turtle.forward(50);
-            //turtle.turnLeft();
-            //turtle.forward(50);
-            //turtle.turnLeft();
-            //turtle.forward(50);
-            //turtle.turnLeft();
-            //turtl.forward(50);
+        turtle2.setPenColor(Color.RED);
+        turtle2.setPenColor(Color.GREEN);
+        Random generator = new Random();
+        while(true)
+        {
+            
+            int random_num = generator.nextInt(16);
+            int random_num2 = generator.nextInt(16);
+            int random_num3 = generator.nextInt(16);
+            turtle.forward(random_num);
+            turtle2.forward(random_num2);
+            turtle3.forward(random_num3);
+            turtle3.turnLeft();
+            turtle2.turnLeft();
+            turtle.turnLeft();
+            turtle.forward(random_num);
+            turtle2.forward(random_num2);
+            turtle3.forward(random_num3);
+            turtle3.turnLeft();
+            turtle2.turnLeft();
+            turtle.turnLeft();
+            turtle.forward(random_num);
+            turtle2.forward(random_num2);
+            turtle3.forward(random_num3);
+            turtle3.turnLeft();
+            turtle2.turnLeft();
+            turtle.turnLeft();
+            turtle.forward(random_num);
+            turtle2.forward(random_num2);
+            turtle3.forward(random_num3);
+            turtle3.turnLeft();
+            turtle2.turnLeft();
+            turtle.turnLeft();
+            turtle.forward(random_num);
+            turtle2.forward(random_num2);
+            turtle3.forward(random_num3);
+            turtle3.turnLeft();
+            turtle2.turnLeft();
+            turtle.turnLeft();
+            turtle.forward(random_num);
+            turtle2.forward(random_num2);
+            turtle3.forward(random_num3);
           
       
         
@@ -30,4 +62,5 @@ public class TurtleWorld
 
         
     }
+}
 }
